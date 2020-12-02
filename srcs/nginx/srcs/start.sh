dc6ce3e560fb110b068333ef6a341fbce31c5172
Paul Correia <paul13.correia@gmail.com>
2020-12-02 22:53:08 +0100

    First commit, nginx,phpmyadmin container done. Work on wordpress container

---
+++ srcs/nginx/srcs/start.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+rm -f /var/cache/apk/*
+
+#create admin user for nginx
+echo -e "passwd\npasswd" | adduser admin
+
+#create necessary directories
+mkdir -p /run/nginx /etc/ssl/certs /etc/ssl/private
+
+#move landing page
+cp /var/lib/nginx/html/index.html /var/www/index.html
+
+#allow ssh connexion
+cd /etc/ssh && ssh-keygen -A
+
+#create ssl credentials
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/certs/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/CN=$MIP"
+openssl dhparam -dsaparam -out /etc/ssl/certs/dhparam.pem 2048
+
+#start services
+#rm /etc/telegraf.conf && telegraf &
+/usr/sbin/nginx -g 'daemon off;'
---
+++ srcs/wordpress/srcs/start.sh
@@ -0,0 +1,3 @@
+php -S 0.0.0.0:5050 -t /www/wordpress
+
+sh
